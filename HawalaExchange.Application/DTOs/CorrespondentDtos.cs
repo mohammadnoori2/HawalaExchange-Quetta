9d10643defc3ece4d6d55b138d384ac6eca1587d
Mustafa Ramazani <mustafaramazani@outlook.com>
2026-06-23 14:08:54 +0430

    Added and Implemented CorrespondentDto.

---
+++ HawalaExchange.Application/DTOs/CorrespondentDtos.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HawalaExchange.Application.DTOs
+{
+    public class CorrespondentDtos
+    {
+        public long Id { get; set; }
+        public string Code { get; set; } = string.Empty;
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public bool IsArchived { get; set; }
+        public string Remarks { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+
+    public class CreateCorrespondentRequest
+    {
+        public string Code { get; set; } = string.Empty;
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string Remarks { get; set; }
+    }
+
+    public class UpdateCorrespondentRequest
+    {
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string Remarks { get; set; }
+    }
+}

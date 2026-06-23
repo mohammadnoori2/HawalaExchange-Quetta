69a5cf1a444eafdff5924da78c503ee93eeab616
Mustafa Ramazani <mustafaramazani@outlook.com>
2026-06-23 14:50:24 +0430

    Added and Implemented ICorrespondednt interface.

---
+++ HawalaExchange.Application/Interfaces/ICorrespondentService.cs
@@ -0,0 +1,16 @@
+﻿using HawalaExchange.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HawalaExchange.Application.Interfaces
+{
+    public interface ICorrespondentService
+    {
+        Task<List<CorrespondentDtos>> GetAllAsync();
+        Task<CorrespondentDtos?> GetByIdAsync(long id);
+        Task<long> CreateAsync(CreateCorrespondentRequest request);
+        Task UpdateAsync(long id, UpdateCorrespondentRequest request);
+        Task DeleteAsync(long id);
+    }
+}

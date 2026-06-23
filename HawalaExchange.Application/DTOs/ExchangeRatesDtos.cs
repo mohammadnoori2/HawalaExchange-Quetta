1c6f05f6e3de37ddcbfd11fcdde717818055be74
Mustafa Ramazani <mustafaramazani@outlook.com>
2026-06-23 15:44:06 +0430

    Added and implemented Dtos.

---
+++ HawalaExchange.Application/DTOs/ExchangeRatesDtos.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HawalaExchange.Application.DTOs
+{
+    public class ExchangeRatesDtos
+    {
+        public long Id { get; set; }
+        public long FromCurrencyId { get; set; }
+        public long ToCurrencyId { get; set; }
+        public decimal BuyRate { get; set; }
+        public decimal SellRate { get; set; }
+        public DateTime EffectiveDate { get; set; }
+        public long CreatedBy { get; set; }
+
+    }
+
+    public class CreateExchangeRatesRequest
+    {
+        public long FromCurrencyId { get; set; }
+        public long ToCurrencyId { get; set; }
+        public decimal BuyRate { get; set; }
+        public decimal SellRate { get; set; }
+        public DateTime EffectiveDate { get; set; }
+        public long CreatedBy { get; set; }
+    }
+
+    public class UpdateExchangeRatesRequest
+    {
+        public long FromCurrencyId { get; set; }
+        public long ToCurrencyId { get; set; }
+        public decimal BuyRate { get; set; }
+        public decimal SellRate { get; set; }
+        public DateTime EffectiveDate { get; set; }
+        public long CreatedBy { get; set; }
+    }
+}

5267df5f1eb709a8d49d9160a10cc91734d9aac2
Mustafa Ramazani <mustafaramazani@outlook.com>
2026-06-23 10:49:54 +0430

    Added Account Interface.

---
+++ HawalaExchange.Application/Interfaces/IAccountService.cs
@@ -0,0 +1,16 @@
+﻿using HawalaExchange.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HawalaExchange.Application.Interfaces
+{
+    public interface IAccountService
+    {
+        Task<List<AccountDtos>> GetAllAsync();
+        Task<AccountDtos?> GetByIdAsync(long id);
+        Task<long> CreateAsync(CreateAccountRequest request);
+        Task UpdateAsync(long id, UpdateAccountRequest request);
+        Task ArchiveAsync(long id);
+    }
+}

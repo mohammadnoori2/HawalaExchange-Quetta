fe9ceda626be17615a2129d2cc236a12bdd87b8b
Mohammad Noori <moh.noori2021@gmail.com>
2026-06-23 09:53:47 +0430

    Registered the CustomerService

--- HawalaExchange.Infrastructure/DependencyInjection.cs
+++ HawalaExchange.Infrastructure/DependencyInjection.cs
@@ -1,4 +1,6 @@
 ﻿
+using HawalaExchange.Application.Interfaces;
+using HawalaExchange.Infrastructure.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -17,6 +19,7 @@ public static class DependencyInjection
 
         services.AddDbContext<ApplicationDbContext>(options =>
             options.UseSqlServer(connectionString));
+        services.AddScoped<ICustomerService, CustomerService>();
 
         return services;
     }

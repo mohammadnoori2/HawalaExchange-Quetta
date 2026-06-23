334630542cd2957b8d09b542a9d8b147d50a0cdd
Mustafa Ramazani <mustafaramazani@outlook.com>
2026-06-23 15:44:26 +0430

    Added and implemented interface.

---
+++ HawalaExchange.Application/Interfaces/IExchangeRatesService.cs
@@ -0,0 +1,16 @@
+﻿using HawalaExchange.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HawalaExchange.Application.Interfaces
+{
+    public interface IExchangeRatesService
+    {
+        public Task<List<ExchangeRatesDtos>> GetAllAsync();
+        public Task<ExchangeRatesDtos?> GetByIdAsync(long id);
+        public Task<long> CreateAsync(CreateExchangeRatesRequest request);
+        public Task UpdateAsync(long id, UpdateExchangeRatesRequest request);
+        public Task DeleteAsync(long id);
+    }
+}

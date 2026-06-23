043bff384e284ec1ce72ad3be88861f9e02f387f
Mustafa Ramazani <mustafaramazani@outlook.com>
2026-06-23 11:22:22 +0430

    Added and Implemented ICurrencyService interface.

---
+++ HawalaExchange.Application/Interfaces/ICurrencyService.cs
@@ -0,0 +1,16 @@
+﻿using HawalaExchange.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HawalaExchange.Application.Interfaces
+{
+    public interface ICurrencyService
+    {
+        Task<List<CurrencyDtos>> GetAllAsync();
+        Task<CurrencyDtos?> GetByIdAsync(long id);
+        Task<long> CreateAsync(CreateCurrencyRequest request);
+        Task UpdateAsync(long id, UpdateCurrencyRequest request);
+        Task DeleteAsync(long id);
+    }
+}

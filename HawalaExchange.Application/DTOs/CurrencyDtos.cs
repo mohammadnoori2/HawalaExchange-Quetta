9db81b9727e6a7661f4758f7e0e616cce626920a
Mustafa Ramazani <mustafaramazani@outlook.com>
2026-06-23 11:06:03 +0430

    Added and Implemented CurrencyDtos.

---
+++ HawalaExchange.Application/DTOs/CurrencyDtos.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HawalaExchange.Application.DTOs
+{
+    public class CurrencyDtos
+    {
+        public long Id { get; set; }
+        public string Code { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Symbol { get; set; } = string.Empty;
+        public int DecimalPlaces { get; set; } = 2;
+        public bool IsActive { get; set; }
+    }
+
+    public class CreateCurrencyRequest
+    {
+        public string Code { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Symbol { get; set; } = string.Empty;
+        public int DecimalPlaces { get; set; } = 2;
+    }
+
+    public class UpdateCurrencyRequest
+    {
+        public string Code { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Symbol { get; set; } = string.Empty;
+        public int DecimalPlaces { get; set; } = 2;
+        public bool IsActive { get; set; }
+    }
+}

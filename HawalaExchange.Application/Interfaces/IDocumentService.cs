e5a3fb56ca503dfd3d07aac3d99c85b63057c151
aref hakimi <arefhakimi2020@gmail.com>
2026-06-30 09:17:53 +0430

    created the documnet page

--- HawalaExchange.Application/Interfaces/IDocumentService.cs
+++ HawalaExchange.Application/Interfaces/IDocumentService.cs
@@ -4,6 +4,8 @@ namespace HawalaExchange.Application.Interfaces.Services
 {
     public interface IDocumentService
     {
+
+        Task<IEnumerable<DocumentDto>> GetAllAsync();
         Task<DocumentDto> UploadDocumentAsync(UploadDocumentDto uploadDto);
         Task<IEnumerable<DocumentDto>> GetDocumentsByEntityAsync(string entityType, long entityId);
         Task<DocumentDto?> GetDocumentByIdAsync(long id);

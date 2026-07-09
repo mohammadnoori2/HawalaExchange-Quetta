3aae93c3503bd12decb36d49964fd43d021563b6
Mustafa Ramazani <mustafaramazani@outlook.com>
2026-07-09 16:49:14 +0430

    Added and impleneted PamentLocation interface.

---
+++ HawalaExchange.Application/Interfaces/IPaymentLocationService.cs
@@ -0,0 +1,15 @@
+﻿using HawalaExchange.Application.DTOs;
+
+namespace HawalaExchange.Application.Interfaces.Services
+{
+    public interface IPaymentLocationService
+    {
+        Task<IEnumerable<PaymentLocationDto>> GetAllAsync();
+        Task<IEnumerable<PaymentLocationDto>> GetActiveAsync();
+        Task<PaymentLocationDto?> GetByIdAsync(long id);
+        Task<PaymentLocationDto> CreateAsync(CreatePaymentLocationDto dto);
+        Task<PaymentLocationDto> UpdateAsync(long id, UpdatePaymentLocationDto dto);
+        Task DeleteAsync(long id);
+        Task<PaymentLocationDto> ToggleActiveAsync(long id);
+    }
+}

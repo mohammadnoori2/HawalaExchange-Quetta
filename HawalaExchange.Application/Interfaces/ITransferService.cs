dda206673cc75305daa571e9b95dafdb349259e6
Mustafa Ramazani <mustafaramazani@outlook.com>
2026-07-10 08:42:24 +0430

    Updated GetTransferByTransactionAsync removed.

--- HawalaExchange.Application/Interfaces/ITransferService.cs
+++ HawalaExchange.Application/Interfaces/ITransferService.cs
@@ -5,7 +5,6 @@ namespace HawalaExchange.Application.Interfaces.Services
     public interface ITransferService
     {
         Task<TransferDto> CreateTransferAsync(CreateTransferDto createDto);
-        Task<IEnumerable<TransferDto>> GetTransfersByTransactionAsync(long transactionId);
         Task<IEnumerable<TransferDto>> GetTransfersByAccountAsync(long accountId);
         Task<IEnumerable<TransferDto>> GetTransfersByMethodAsync(string transferMethod);
         Task<TransferDto?> GetTransferByIdAsync(long id);
